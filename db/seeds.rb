7a34d7bb3ff7552bcf025dd5eaaace6cf43bad0a
bitkoda <jf.liv@pm.me>
2023-06-02 04:19:33 +0100

    update seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,11 @@ Movie.create!([
       once more in order to undo Thanos' actions and restore order to the universe.
     ).squish,
                   released_on: '2019-04-26',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 1_223_641_414,
                   director: 'Anthony Russo',
-                  duration: 181
+                  duration: 181,
+                  tagline: 'Avenge the fallen.'
                 },
                 {
                   title: 'Captain Marvel',
@@ -23,10 +24,11 @@ Movie.create!([
       war between two alien races.
     ).squish,
                   released_on: '2019-03-08',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 1_110_662_849,
                   director: 'Anna Boden',
-                  duration: 124
+                  duration: 124,
+                  tagline: 'Higher. Further. Faster.'
                 },
                 {
                   title: 'Black Panther',
@@ -36,10 +38,11 @@ Movie.create!([
       future and must confront a challenger from his country's past.
     ).squish,
                   released_on: '2018-02-16',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 1_346_913_161,
                   director: 'Ryan Coogler',
-                  duration: 134
+                  duration: 134,
+                  tagline: 'Long live the king.'
                 },
                 {
                   title: 'Avengers: Infinity War',
@@ -49,10 +52,11 @@ Movie.create!([
       his blitz of devastation and ruin puts an end to the universe.
     ).squish,
                   released_on: '2018-04-27',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 2_048_359_754,
                   director: 'Anthony Russo',
-                  duration: 149
+                  duration: 149,
+                  tagline: 'An entire universe. Once and for all.'
                 },
                 {
                   title: 'Green Lantern',
@@ -62,10 +66,11 @@ Movie.create!([
       him into an intergalactic police force, the Green Lantern Corps.
     ).squish,
                   released_on: '2011-06-17',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 219_851_172,
                   director: 'Martin Campbell',
-                  duration: 114
+                  duration: 114,
+                  tagline: 'In our darkest hour, there will be light.'
                 },
                 {
                   title: 'Fantastic Four',
@@ -76,10 +81,11 @@ Movie.create!([
       turned enemy.
     ).squish,
                   released_on: '2015-08-07',
-                  rating: 'PG-13',
+                  rating: '15',
                   total_gross: 168_257_860,
                   director: 'Josh Trank',
-                  duration: 100
+                  duration: 100,
+                  tagline: 'Change is coming.'
                 },
                 {
                   title: 'Iron Man',
@@ -90,10 +96,11 @@ Movie.create!([
       decides to use its technology to fight against evil.
     ).squish,
                   released_on: '2008-05-02',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 585_366_247,
                   director: 'Jon Favreau',
-                  duration: 126
+                  duration: 126,
+                  tagline: "Heroes aren't born. They're built."
                 },
                 {
                   title: 'Superman',
@@ -107,7 +114,8 @@ Movie.create!([
                   rating: 'PG',
                   total_gross: 300_451_603,
                   director: 'Richard Donner',
-                  duration: 143
+                  duration: 143,
+                  tagline: "You'll believe a man can fly."
                 },
                 {
                   title: 'Spider-Man',
@@ -119,10 +127,11 @@ Movie.create!([
       befalls his family.
     ).squish,
                   released_on: '2002-05-03',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 825_025_036,
                   director: 'Sam Raimi',
-                  duration: 121
+                  duration: 121,
+                  tagline: 'With great power comes great responsibility.'
                 },
                 {
                   title: 'Batman',
@@ -132,10 +141,11 @@ Movie.create!([
       first major enemy being the clownishly homicidal Joker.
     ).squish,
                   released_on: '1989-06-23',
-                  rating: 'PG-13',
+                  rating: '15',
                   total_gross: 411_348_924,
                   director: 'Tim Burton',
-                  duration: 126
+                  duration: 126,
+                  tagline: 'Have you ever danced with the devil in the pale moonlight?'
                 },
                 {
                   title: 'Catwoman',
@@ -145,10 +155,11 @@ Movie.create!([
       she has a more than respectable career as a graphic designer.
     ).squish,
                   released_on: '2004-07-23',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 82_102_379,
                   director: "Jean-Christophe 'Pitof' Comar",
-                  duration: 101
+                  duration: 101,
+                  tagline: 'You can’t CATch her.'
                 },
                 {
                   title: 'Wonder Woman',
@@ -158,40 +169,151 @@ Movie.create!([
       home to fight a war, discovering her full powers and true destiny.
     ).squish,
                   released_on: '2017-06-02',
-                  rating: 'PG-13',
+                  rating: '12A',
                   total_gross: 821_847_012,
                   director: 'Patty Jenkins',
-                  duration: 141
+                  duration: 141,
+                  tagline: 'Power. Grace. Wisdom. Wonder.'
+                },
+                {
+                  title: 'Daredevil',
+                  description:
+                  %(
+                  A man blinded in a childhood accident fights
+                  crime using his superhumanly-elevated remaining
+                  senses.).squish,
+                  released_on: '2003-02-14',
+                  rating: '15',
+                  total_gross: 179_179_718,
+                  director: 'Mark Steven Johnson',
+                  duration: 103,
+                  tagline: 'When the streets have gone to Hell, have faith in the devil.'
+                },
+                {
+                  title: 'Hulk',
+                  description:
+                  %(
+                  Bruce Banner, a genetics researcher with a tragic past,
+                  suffers massive radiation exposure in his laboratory that
+                  causes him to transform into a raging green monster when he
+                  gets angry.).squish,
+                  released_on: '2003-07-18',
+                  rating: '12A',
+                  total_gross: 245_360_480,
+                  director: 'Ang Lee',
+                  duration: 141,
+                  tagline: 'What if everything you were forced to keep inside was suddenly set free?'
+                },
+                {
+                  title: 'Ghost Rider',
+                  description:
+                  %(In order to save his dying father, young stunt cyclist
+                  Johnny Blaze sells his soul to Mephistopheles and sadly
+                  parts from the pure-hearted Roxanne Simpson, the love of
+                  his life. Years later, Johnny's path crosses again with
+                  Roxanne, now a go-getting reporter, and also with
+                  Mephistopheles, who offers to release Johnny's soul if
+                  Johnny becomes the fabled, fiery 'Ghost Rider'.).squish,
+                  released_on: '2007-03-01',
+                  rating: '15',
+                  total_gross: 228_738_393,
+                  director: 'Mark Steven Johnson',
+                  duration: 114,
+                  tagline: 'Hell is about to be unleashed.'
                 }
               ])
 
-genres = %w[sci-fi
-            comedy
-            adventure
-            action
-            horror
-            romance
-            thriller]
+movie_genres = %w[action
+                  adventure
+                  comedy
+                  crime
+                  fantasy
+                  horror
+                  mystery
+                  romance
+                  sci-fi
+                  thriller]
 
-genres.each do |genre|
+movie_genres.each do |genre|
   Genre.create!(name: genre)
 end
 
 [
-  ['Avengers: Endgame', 'avengers-end-game.png'],
-  ['Avengers: Infinity War', 'avengers-infinity-war.png'],
-  ['Batman', 'batman.png'],
-  ['Black Panther', 'black-panther.png'],
-  ['Captain Marvel', 'captain-marvel.png'],
-  ['Catwoman', 'catwoman.png'],
-  ['Fantastic Four', 'fantastic-four.png'],
-  ['Green Lantern', 'green-lantern.png'],
-  ['Iron Man', 'ironman.png'],
-  ['Spider-Man', 'spiderman.png'],
-  ['Superman', 'superman.png'],
-  ['Wonder Woman', 'wonder-woman.png']
+  ['Avengers: Endgame', 'avengers-endgame.jpg', %w[action adventure sci-fi]],
+  ['Avengers: Infinity War', 'avengers-infinity-war.jpg', %w[action adventure sci-fi]],
+  ['Batman', 'batman.jpg', %w[fantasy action crime]],
+  ['Black Panther', 'black-panther.jpg', %w[action adventure sci-fi]],
+  ['Captain Marvel', 'captain-marvel.jpg', %w[action adventure sci-fi]],
+  ['Catwoman', 'catwoman.jpg', %w[action crime fantasy mystery]],
+  ['Daredevil', 'daredevil.jpg', %w[action fantasy]],
+  ['Fantastic Four', 'fantastic-four.jpg', %w[action adventure sci-fi]],
+  ['Ghost Rider', 'ghost-rider.jpg', %w[action fantasy thriller]],
+  ['Green Lantern', 'green-lantern.jpg', %w[action adventure sci-fi thriller]],
+  ['Hulk', 'hulk.jpg', %w[action adventure sci-fi]],
+  ['Iron Man', 'ironman.jpg', %w[action adventure sci-fi]],
+  ['Spider-Man', 'spider-man.jpg', %w[action fantasy]],
+  ['Superman', 'superman.jpg', %w[action adventure sci-fi]],
+  ['Wonder Woman', 'wonder-woman.jpg', %w[action adventure fantasy]]
 ].each do |title, filename|
   movie = Movie.find_by(title:)
   file = File.open(Rails.root.join("app/assets/images/#{filename}"))
   movie.main_image.attach(io: file, filename:)
 end
+
+[
+  ['Avengers: Endgame', %w[action adventure sci-fi]],
+  ['Avengers: Infinity War', %w[action adventure sci-fi]],
+  ['Batman', %w[fantasy action crime]],
+  ['Black Panther', %w[action adventure sci-fi]],
+  ['Captain Marvel', %w[action adventure sci-fi]],
+  ['Catwoman', %w[action crime fantasy mystery]],
+  ['Daredevil', %w[action fantasy]],
+  ['Fantastic Four', %w[action adventure sci-fi]],
+  ['Ghost Rider', %w[action fantasy thriller]],
+  ['Green Lantern', %w[action adventure sci-fi thriller]],
+  ['Hulk', %w[action adventure sci-fi]],
+  ['Iron Man', %w[action adventure sci-fi]],
+  ['Spider-Man', %w[action fantasy]],
+  ['Superman', %w[action adventure sci-fi]],
+  ['Wonder Woman', %w[action adventure fantasy]]
+].each do |title, genres|
+  movie = Movie.find_by(title:)
+  genres.each do |genre|
+    movie.genres << genre
+  end
+end
+
+User.create!([
+               {
+                 name: 'Tony Stark',
+                 email: 'ironman@marvel.com',
+                 username: 'ironfist',
+                 password: 'secret_ironman',
+                 password_confirmation: 'secret_ironman',
+                 admin: true
+               },
+               {
+                 name: 'Eric Banner',
+                 email: 'hulk@marvel.com',
+                 username: 'greenmachine',
+                 password: 'secret_greenmachine',
+                 password_confirmation: 'secret_greenmachine',
+                 admin: false
+               },
+               {
+                 name: 'Patience Philips',
+                 email: 'catwoman@marvel.com',
+                 username: 'sexycat',
+                 password: 'secret_sexycat',
+                 password_confirmation: 'secret_sexycat',
+                 admin: false
+               },
+               {
+                 name: 'Carol Danvers',
+                 email: 'captainmarvel@marvel.com',
+                 username: 'marvellous',
+                 password: 'secret_captainmarvel',
+                 password_confirmation: 'secret_captainmarvel',
+                 admin: false
+               }
+             ])
