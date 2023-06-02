fc6068c2e82e7ff1ff2246cc863b9f93709feab5
bitkoda <jf.liv@pm.me>
2023-06-02 02:27:39 +0100

    fix broken image link in genres index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@
 
 # ApplicationHelper
 module ApplicationHelper
+  def main_image(movie)
+    if movie.main_image.attached?
+      image_tag(movie.main_image,
+                class: 'card-img-top')
+    else
+      image_tag('placeholder.png', class: 'card-img-top')
+    end
+  end
 end
